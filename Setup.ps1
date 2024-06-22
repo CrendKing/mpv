faf67660c029c72b504aabca85f9c3112df2663a
Crend King <975235+CrendKing@users.noreply.github.com>
2024-06-22 04:07:18 -0700

    My

---
+++ Setup.ps1
@@ -0,0 +1,9 @@
+& 'D:\Script\vs_developer_shell.ps1'
+& "${PSScriptRoot}\..\fix_meson\venv\Scripts\Activate.ps1"
+
+$env:CC = 'clang'
+$env:CXX = 'clang++'
+$env:CC_LD = 'lld'
+$env:CXX_LD = 'lld'
+$env:WINDRES = 'llvm-rc'
+$env:PATH += ";${PSScriptRoot}\CMake\bin;D:\Scoop\apps\git\current\usr\bin"
